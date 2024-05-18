23631a45425ed5b366c475df4159663cc6e74e06
Subhistar <subhisuper@gmail.com>
2024-05-18 10:08:31 -0400

    frjebkgtfvhedskl,

--- audio_recorder.py
+++ audio_recorder.py
@@ -2,10 +2,8 @@ import streamlit as st
 from streamlit_mic_recorder import mic_recorder, speech_to_text
 import io
 import os
-# from whisper_stt import whisper_stt
 
 state = st.session_state
-# openai.api_key = 'sk-proj-eHstyl2tLbaZWB3LWIHaT3BlbkFJl17l1Py5OiLbNxloVQlo'
 
 if 'text_received' not in state:
     state.text_received = []
@@ -30,10 +28,3 @@ audio = mic_recorder(start_prompt="Start Recording", stop_prompt="Stop Recording
 if audio:
     state.audio_data = audio['bytes']
     st.audio(audio['bytes'])
-
-#from pydub import AudioSegment
-#AudioSegment.from_file("/input/file").export("/output/file", format="mp3")
-
-# text = whisper_stt(openai_api_key="sk-proj-eHstyl2tLbaZWB3LWIHaT3BlbkFJl17l1Py5OiLbNxloVQlo", language = 'en')  
-# if text:
-#     st.write(text)
